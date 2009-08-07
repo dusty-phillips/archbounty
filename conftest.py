059bebb77a6df4a101b01247a1c3f51d9e4aed09
Dusty Phillips <buchuki@gmail.com>
2009-08-07 15:24:27 -0400

    Clear database after each test.

--- conftest.py
+++ conftest.py
@@ -4,8 +4,9 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from django.conf import settings
 from django.test.client import Client
 from django.test.utils import setup_test_environment, teardown_test_environment
+from django.core.management import call_command
 
-def pytest_funcarg__client(request):
+def pytest_funcarg__django_client(request):
     old_name = settings.DATABASE_NAME
     def setup():
         setup_test_environment()
@@ -18,3 +19,10 @@ def pytest_funcarg__client(request):
         from django.db import connection
         connection.creation.destroy_test_db(old_name, 1)
     return request.cached_setup(setup, teardown, "session")
+
+def pytest_funcarg__client(request):
+    def setup():
+        return request.getfuncargvalue('django_client')
+    def teardown(client):
+        call_command('flush', verbosity=0, interactive=False)
+    return request.cached_setup(setup, teardown, "function")
