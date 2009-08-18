3fa229ad3e69e02d6f57d2a76e283389550a5cd6
Dusty Phillips <buchuki@gmail.com>
2009-08-18 13:36:27 -0600

    Add nested comments to the view_project page.

--- settings.py
+++ settings.py
@@ -58,6 +58,15 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.load_template_source',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request")
+
+
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -75,6 +84,8 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.comments',
     'registration',
     'bounty',
 )
--- urls.py
+++ urls.py
@@ -6,11 +6,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
         (r'^admin/', include(admin.site.urls)),
         (r'^accounts/', include('registration.urls')),
+        (r'^comments/', include('django.contrib.comments.urls')),
         (r'^$', 'bounty.views.index'),
         (r'^new_project', 'bounty.views.new_project'),
         (r'^projects/(?P<project_id>\d+)/$', 'bounty.views.view_project'),
         (r'^projects/$', 'bounty.views.list_projects'),
         (r'^projects/(?P<project_status>pending|accepted)/$',
             'bounty.views.list_projects'),
-
 )
