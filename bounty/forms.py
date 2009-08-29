57050c85f9e0c23c3d24c168a41bdc0fb1b2f184
Dusty Phillips <buchuki@gmail.com>
2009-08-28 23:41:40 -0600

    Add a form for Contributions.

--- bounty/forms.py
+++ bounty/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from bounty.models import Project
+from bounty.models import Project, Contribution
 
 class ProjectForm(forms.ModelForm):
     class Meta:
@@ -13,3 +13,8 @@ class ProjectStatusForm(forms.ModelForm):
 
 class DonationForm(forms.Form):
     amount = forms.DecimalField()
+
+class ContributionForm(forms.ModelForm):
+    class Meta:
+        model = Contribution
+        fields = ('percentage', 'description')
